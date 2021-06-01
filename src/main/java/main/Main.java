6e476f19f4f6392f779a37812e8a3cf432a0eeb7
SeanAnica <37900784+SeanAnica@users.noreply.github.com>
2021-06-01 15:50:16 +0200

    primefaces

--- src/main/java/main/Main.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class Main {
-
-	public static void main(String[] args) {
-		//Dao dao = new Dao();//permet de lancer la persistence des données et de générer les tables dans la base de données.
-	}
-
-}
